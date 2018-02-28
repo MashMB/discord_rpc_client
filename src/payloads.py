e3859623107fad0e4a778fabfb51e58e5df79d8f
Mash <mbedra666@gmail.com>
2018-02-28 20:26:33 +0100

    Adding description of Rich Presence payload

--- src/payloads.py
+++ src/payloads.py
@@ -11,3 +11,37 @@ handshake = {
 	"v": 1,
 	"client_id": None
 }
+
+# Json template for rpc assets section
+rpc_assets = {
+	"large_text": None,
+	"large_image": None,
+	"small_text": None,
+	"small_image": None
+}
+
+# Json template for rpc timestamps section
+rpc_timestamps = {
+	"start": None
+}
+
+# Json template for rpc activity section
+rpc_activity = {
+	"details": None,
+	"state": None,
+	"timestamps": rpc_timestamps,
+	"asstes": rpc_assets
+}
+
+# Json template for rpc args section
+rpc_args = {
+	"activity": rpc_activity,
+	"pid": None
+}
+
+# Payload template for Discord Rich Presence
+rpc = {
+	"cmd": "SET_ACTIVITY",
+	"args": rpc_args,
+	"nonce": None
+}
