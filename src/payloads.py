45d7b80adc6f1e2e9a7fd202838e81a9c14d7c35
Mash <mbedra666@gmail.com>
2018-02-28 13:32:07 +0100

    Adding description of handshake payload

--- src/payloads.py
+++ src/payloads.py
@@ -5,3 +5,9 @@ Author: Maciej Bedra
 File that contains description of payloads that
 can be send to Discord app via Discrod IPC socket.
 """
+
+# Payload for handshaking
+handshake = {
+	"v": 1,
+	"client_id": None
+}
