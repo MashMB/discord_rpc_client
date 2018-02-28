4a3780e1ccfdf3263e4d38460a53a3ffed30fc5d
Mash <mbedra666@gmail.com>
2018-02-28 15:02:57 +0100

    Refactoring payloads.py

--- src/payloads.py
+++ src/payloads.py
@@ -2,11 +2,11 @@
 File name: payloads.py
 Author: Maciej Bedra
 
-File that contains description of payloads that
+File that contains templates of payloads that
 can be send to Discord app via Discrod IPC socket.
 """
 
-# Payload for handshaking
+# Payload template for handshaking
 handshake = {
 	"v": 1,
 	"client_id": None
