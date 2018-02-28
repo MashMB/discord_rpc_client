7fef7957bee23034732be98781b4ddabd7b90072
Mash <mbedra666@gmail.com>
2018-02-28 16:55:20 +0100

    Updating logger appearance in discord_ipc.py

--- src/discord_ipc.py
+++ src/discord_ipc.py
@@ -17,7 +17,7 @@ import struct
 import sys
 
 # Configuring logger
-logging.basicConfig(format = "%(asctime)s: %(levelname)s: %(message)s", datefmt = "%d.%m.%Y (%H:%M:%S)", level = logging.DEBUG)
+logging.basicConfig(format = "%(asctime)s: [%(levelname)s]: %(message)s", datefmt = "%d.%m.%Y (%H:%M:%S)", level = logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 class DiscordIPC:
