d3d4bf66dbeb888e909958985a193fd1ade941ba
Mash <mbedra666@gmail.com>
2018-03-13 20:04:44 +0100

    Method to get system name and eventually raise exception if OS unsupported

--- src/discord_ipc.py
+++ src/discord_ipc.py
@@ -3,10 +3,13 @@ File name: discord_ipc.py
 Author: Maciej Bedra
 
 Simple Discord IPC wrapper that gives opportunity 
-to use Discord Rich Presence for example.
+to use Discord Rich Presence.
 """
 
 import logging
+import os_dependencies
+import platform
+import sys
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format = "%(asctime)s: [%(levelname)s]: %(message)s", datefmt = "%d.%m.%Y (%H:%M:%S)", level = logging.DEBUG)
@@ -26,5 +29,33 @@ class DiscordIPC:
 		:type client_id: string
 		"""
 
+		try:
+			self.system_name = self.get_system_name()
+		except Exception:
+			logger.warning("Unsupported OS")
+			sys.exit(1)
+
 		self.client_id = client_id
 		self.is_connected = False
+
+	def get_system_name(self):
+		"""
+		Getting system name and checking if running OS
+		is supported. Supported OS: Windows, Linux MacOS.
+
+		:returns: running OS name (lowercase)
+		:rtype: string
+		:raise Exception: if running OS is not included in supported
+		OS list, raise the Exception
+		"""
+
+		logger.info("Recognizing running OS...")
+		system_name = platform.system()
+		logger.info("Running OS: " + system_name)
+		system_name = system_name.lower()
+		
+		if system_name in os_dependencies.supported:
+			logger.info("Supported OS")
+			return system_name
+		else:
+			raise Exception("Unsupported OS")
