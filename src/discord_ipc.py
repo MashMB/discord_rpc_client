74d0b1bb05683cd42fdf4b5b179bec2fd2f340d4
Mash <mbedra666@gmail.com>
2018-03-13 19:23:41 +0100

    Configure logger

--- src/discord_ipc.py
+++ src/discord_ipc.py
@@ -5,3 +5,8 @@ Author: Maciej Bedra
 Simple Discord IPC wrapper that gives opportunity 
 to use Discord Rich Presence for example.
 """
+
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format = "%(asctime)s: [%(levelname)s]: %(message)s", datefmt = "%d.%m.%Y (%H:%M:%S)", level = logging.DEBUG)
