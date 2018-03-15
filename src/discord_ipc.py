e168d43472a859bd3a0695abc3acc8a5cd811780
Mash <mbedra666@gmail.com>
2018-03-15 18:05:58 +0100

    Remove unnecessary thread to keep connection alive

--- src/discord_ipc.py
+++ src/discord_ipc.py
@@ -45,7 +45,6 @@ class DiscordIPC:
 		self.client_id = client_id
 		self.is_connected = False
 		self.pipe = None
-		self.discord_listener = threading.Thread(name = "discord_listener", target = self.keep_connection_alive)
 		self.ipc_socket = self.get_ipc_socket()
 
 	def get_current_time(self):
@@ -139,15 +138,14 @@ class DiscordIPC:
 			self.send_data(0, payloads.handshake)
 			self.read_data()
 			self.is_connected = True
-			self.discord_listener.start()
 			logger.info("Connection with Discord established")
 		else:
 			logger.warning("Already connected to Discord")
 
 	def disconnect(self):
 		"""
-		Disconnecting from Discord by terminating thread
-		that keeps connection alive and closing pipe connection.
+		Disconnecting from Discord by sending close 
+		signal to Discord and closing pipe connection.
 		"""
 
 		logger.info("Disconnecting from Discord...")
@@ -155,15 +153,6 @@ class DiscordIPC:
 		self.pipe.close()
 		self.is_connected = False
 		logger.info("Disconnected")
-
-	def keep_connection_alive(self):
-		"""
-		Simple mechanism to keep connection
-		alive by thread.
-		"""
-
-		while self.is_connected:
-			time.sleep(0.1)
 
 	def read_data(self):
 		"""
